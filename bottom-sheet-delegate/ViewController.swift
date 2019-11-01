a5e4c3c5e65c92ab31988903e91a0c1d0850643b
Henrik Brustad <henrikbrustad@gmail.com>
2019-11-01 15:24:22 +0100

    Initial Commit

---
+++ bottom-sheet-delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bottom-sheet-delegate
+//
+//  Created by Granheim Brustad , Henrik on 01/11/2019.
+//  Copyright © 2019 Henrik Brustad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bottom-sheet-delegateTests/bottom_sheet_delegateTests.swift
@@ -0,0 +1,34 @@
+//
+//  bottom_sheet_delegateTests.swift
+//  bottom-sheet-delegateTests
+//
+//  Created by Granheim Brustad , Henrik on 01/11/2019.
+//  Copyright © 2019 Henrik Brustad. All rights reserved.
+//
+
+import XCTest
+@testable import bottom_sheet_delegate
+
+class bottom_sheet_delegateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
